93ff0ce39e9e6bb1daf42c5c748093ef58af9df3
barbarapasalic <barb.pasalic@gmail.com>
2023-12-18 02:11:41 +0100

    Update version

--- src/createlist.js
+++ src/createlist.js
@@ -1,85 +1,138 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const shoppingListForm = document.getElementById('shoppingListForm');
-    const itemsList = document.getElementById('itemsList');
-    const itemNameInput = document.getElementById('itemName');
-    const addItemButton = document.getElementById('addItem');
-    const createdList = document.getElementById('createdList');
-    const displayedTitle = document.getElementById('displayedTitle');
-    const displayedItems = document.getElementById('displayedItems');
-    const openCameraButton = document.getElementById('openCamera');
+import { set } from "https://cdn.jsdelivr.net/npm/idb-keyval@6/+esm";
+document.addEventListener("DOMContentLoaded", function () {
+  const shoppingListForm = document.getElementById("shoppingListForm");
+  const itemsList = document.getElementById("itemsList");
+  const itemNameInput = document.getElementById("itemName");
+  const addItemButton = document.getElementById("addItem");
+  const createdList = document.getElementById("createdList");
+  const displayedTitle = document.getElementById("displayedTitle");
+  const displayedItems = document.getElementById("displayedItems");
+  const openCameraButton = document.getElementById("openCamera");
 
-    addItemButton.addEventListener('click', function () {
-        const itemName = itemNameInput.value.trim();
-        if (itemName !== '') {
-            const listItem = document.createElement('li');
-            listItem.textContent = itemName;
-            itemsList.appendChild(listItem);
-            itemNameInput.value = '';
+  let shoppingListItems = [];
+
+  addItemButton.addEventListener("click", function () {
+    const itemName = itemNameInput.value.trim();
+    if (itemName !== "") {
+      const listItem = document.createElement("li");
+      listItem.textContent = itemName;
+      itemsList.appendChild(listItem);
+      itemNameInput.value = "";
+      shoppingListItems.push({ text: itemName });
+    }
+  });
+
+  shoppingListForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(shoppingListItems)
+    const listTitle = document.getElementById("listTitle").value;
+
+    // Save the entire shoppingListItems array instead of just text content
+    saveShoppingList({ title: listTitle, items: shoppingListItems });
+
+    window.location.replace("index.html");
+  });
+
+  function saveShoppingList(list) {
+    console.log(list)
+    set(list.title, list.items)
+      .then(() => console.log('List saved!'))
+      .catch(err => console.error('Failed to save list', err));
+  }
+
+  let cameraStream = null;
+
+  openCameraButton.addEventListener("click", () => {
+    openCamera();
+  });
+
+  const captureButton = document.getElementById("captureImage");
+  captureButton.addEventListener("click", captureImage);
+
+  function openCamera() {
+    const constraints = { video: true };
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then((stream) => {
+        cameraStream = stream;
+        const videoElement = document.getElementById("cameraStream");
+        if (!videoElement) {
+          console.error("Video element not found");
+          return;
         }
-    });
-
-    shoppingListForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        const listTitle = document.getElementById('listTitle').value;
-        const listItems = Array.from(itemsList.children).map(item => item.textContent);
-
-        saveShoppingList({ title: listTitle, items: listItems });
-
-        // Redirect to the home page after creating the list
-        window.location.replace('index.html');
-    });
-
-    function saveShoppingList(list) {
-        const request = indexedDB.open('ShoppingListsDB', 1);
-
-        request.onupgradeneeded = function (event) {
-            const db = event.target.result;
-            const objectStore = db.createObjectStore('shoppingLists', { keyPath: 'id', autoIncrement: true });
-            objectStore.add(list);
+        videoElement.srcObject = stream;
+        videoElement.onloadedmetadata = () => {
+          videoElement.play();
+          captureButton.hidden = false;
         };
+        videoElement.hidden = false;
+      })
+      .catch((error) => {
+        console.error("Error accessing the camera: ", error);
+        // Show the image upload input if camera access is denied
+        document.getElementById("imageUpload").style.display = "block";
+      });
+  }
 
-        request.onsuccess = function (event) {
-            const db = event.target.result;
-            const transaction = db.transaction(['shoppingLists'], 'readwrite');
-            const objectStore = transaction.objectStore('shoppingLists');
-            objectStore.add(list);
-        };
-
-        request.onerror = function (event) {
-            console.error('Error opening IndexedDB:', event.target.error);
-        };
+  function captureImage() {
+    const videoElement = document.getElementById("cameraStream");
+    if (!videoElement) {
+      console.error("Video element not found for capture");
+      return;
     }
 
-    openCameraButton.addEventListener('click', () => {
-        openCamera();
-    });
+    const canvas = document.createElement("canvas");
+    canvas.width = videoElement.videoWidth;
+    canvas.height = videoElement.videoHeight;
 
-    function openCamera() {
-        const constraints = { video: true };
-        navigator.mediaDevices.getUserMedia(constraints)
-            .then((stream) => {
-                const videoElement = document.getElementById('cameraStream');
-                videoElement.srcObject = stream;
-                videoElement.hidden = false;
+    canvas.getContext("2d").drawImage(videoElement, 0, 0);
+    const capturedImage = document.getElementById("capturedImage");
+    if (!capturedImage) {
+      console.error("Captured image element not found");
+      return;
+    }
+    //capturedImage.src = canvas.toDataURL("image/png");
+    capturedImage.hidden = false;
 
-                setTimeout(captureImage, 3000); // Capture image after 3 seconds
-            })
-            .catch((error) => {
-                console.error('Error accessing the camera: ', error);
-            });
+    if (cameraStream) {
+      const tracks = cameraStream.getTracks();
+      tracks.forEach((track) => track.stop());
     }
 
-    function captureImage() {
-        const videoElement = document.getElementById('cameraStream');
-        const canvas = document.createElement('canvas');
-        canvas.width = videoElement.videoWidth;
-        canvas.height = videoElement.videoHeight;
-        canvas.getContext('2d').drawImage(videoElement, 0, 0);
+    videoElement.hidden = true;
+    captureButton.hidden = true;
 
-        const capturedImage = document.getElementById('capturedImage');
-        capturedImage.src = canvas.toDataURL('image/png');
-        capturedImage.hidden = false;
-        videoElement.hidden = true;
-    }
+    const imageData = canvas.toDataURL("image/png");
+    shoppingListItems.push({ image: imageData });
+
+
+    const listItem = document.createElement('li');
+    const imageElement = document.createElement('img');
+    imageElement.src = imageData;
+    imageElement.width = 200;
+    imageElement.height = 200;
+    listItem.appendChild(imageElement);
+    itemsList.appendChild(listItem);
+
+  }
+});
+const imageUploadInput = document.getElementById("imageUpload");
+imageUploadInput.addEventListener('change', function (event) {
+  const file = event.target.files[0];
+  if (file && file.type.startsWith('image/')) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const imageData = e.target.result;
+      shoppingListItems.push({ image: imageData });
+
+      const listItem = document.createElement('li');
+      const imageElement = new Image();
+      imageElement.src = imageData;
+      imageElement.width = 200;
+      imageElement.height = 200;
+      listItem.appendChild(imageElement);
+      itemsList.appendChild(listItem);
+    };
+    reader.readAsDataURL(file);
+  }
 });
--- src/main.js
+++ src/main.js
@@ -1,69 +1,55 @@
+import { keys, get } from "https://cdn.jsdelivr.net/npm/idb-keyval@6/+esm";
+
 document.addEventListener('DOMContentLoaded', function () {
     const mainContent = document.getElementById('mainContent');
 
-    const request = indexedDB.open('ShoppingListsDB', 1);
-
-    request.onsuccess = function () {
-        const db = request.result;
-        if (db.objectStoreNames.contains('shoppingLists')) {
-            // Database is initialized, proceed to retrieve and display shopping lists
-            getShoppingLists().then(savedLists => {
-                if (savedLists.length === 0) {
-                    // If no lists, display a welcome message
-                    mainContent.innerHTML = '<p>Welcome to the homepage!</p>';
-                } else {
-                    // If lists exist, display them
-                    savedLists.forEach((list) => {
-                        const shoppingListDiv = document.createElement('div');
-                        shoppingListDiv.classList.add('shopping-list');
-
-                        const listTitle = document.createElement('h2');
-                        listTitle.textContent = list.title;
-                        shoppingListDiv.appendChild(listTitle);
-
-                        list.items.forEach((itemText) => {
-                            // ... (your existing code)
-                        });
-
-                        mainContent.appendChild(shoppingListDiv);
-                    });
-                }
-            });
+    getShoppingLists().then(savedLists => {
+        console.log(savedLists)
+        if (savedLists.length === 0) {
+            mainContent.innerHTML = '<p>Welcome to the homepage! No lists found.</p>';
         } else {
-            // Database is not initialized, display a welcome message
-            mainContent.innerHTML = '<p>Welcome to the homepage!</p>';
+            savedLists.forEach((list) => {
+                const shoppingListDiv = document.createElement('div');
+                shoppingListDiv.classList.add('shopping-list');
+
+                const listTitle = document.createElement('h2');
+                listTitle.textContent = list.title;
+                shoppingListDiv.appendChild(listTitle);
+
+                const itemsList = document.createElement('ul');
+                list.items.forEach((item) => {
+                    const listItem = document.createElement('li');
+
+                    if (item.image) {
+                        const image = new Image();
+                        image.src = item.image;
+                        image.width = 200;
+                        image.height = 200;
+                        listItem.appendChild(image);
+                    } else if (item.text) {
+                        listItem.textContent = item.text;
+                    }
+
+                    itemsList.appendChild(listItem);
+                });
+                shoppingListDiv.appendChild(itemsList);
+
+                mainContent.appendChild(shoppingListDiv);
+            });
         }
-    };
-
-    request.onerror = function (event) {
-        console.error('Error opening IndexedDB:', event.target.error);
-    };
-
-
-    function getShoppingLists() {
-        return new Promise((resolve, reject) => {
-            const request = indexedDB.open('ShoppingListsDB', 1);
-
-            request.onsuccess = function (event) {
-                const db = event.target.result;
-                console.log(db)
-                const transaction = db.transaction(['shoppingLists'], 'readonly');
-                const objectStore = transaction.objectStore('shoppingLists');
-                const getAllRequest = objectStore.getAll();
-
-                getAllRequest.onsuccess = function (event) {
-                    resolve(event.target.result || []);
-                };
-
-                getAllRequest.onerror = function (event) {
-                    reject(event.target.error);
-                };
-            };
-
-            request.onerror = function (event) {
-                console.error('Error opening IndexedDB:', event.target.error);
-                reject(event.target.error);
-            };
-        });
-    }
+    }).catch(error => {
+        console.error('Error fetching lists:', error);
+        mainContent.innerHTML = '<p>Error loading lists.</p>';
+    });
 });
+function getShoppingLists() {
+    return new Promise((resolve, reject) => {
+        keys()
+            .then(allKeys => {
+                Promise.all(allKeys.map(key => get(key)))
+                    .then(results => resolve(results.map((items, index) => ({ title: allKeys[index], items }))))
+                    .catch(err => reject(err));
+            })
+            .catch(err => reject(err));
+    });
+}
